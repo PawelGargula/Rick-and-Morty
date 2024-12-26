7181ec362318bcaac099850e9de640a66b5cffa8
Paweł Gargula <pawel.gargula@interia.pl>
2024-12-26 12:31:42 +0100

    create template for project

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <>
+      <main>
+        <h1 className="text-3xl font-bold underline">Rick and Morty characters</h1>
+      </main>
+    </>
+  )
+}
+
+export default App
